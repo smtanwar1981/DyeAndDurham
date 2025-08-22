8b442f03cdefb24073bb94e86111254872a8ecaf
Sandeep Tanwar <sandeep.tanwar@nexpay.com.au>
2025-08-22 10:39:16 +1000

    refactoring

--- DyeAndDurham.Sorter.Core/Sorting/PersonNameComparer.cs
+++ DyeAndDurham.Sorter.Core/Sorting/PersonNameComparer.cs
@@ -10,24 +10,26 @@ public sealed class PersonNameComparer : IComparer<PersonName>
     {
         _comp = comp ?? StringComparer.OrdinalIgnoreCase;   
     }
-    public int Compare(PersonName? x, PersonName? y)
+    public int Compare(PersonName? person1, PersonName? person2)
     {
-        if (ReferenceEquals(x, y)) return 0;
-        if (x is null) return -1;
-        if (y is null) return 1;
-        
-        var c = _comp.Compare(x.LastName, y.LastName);
-        if(c != 0) return c;
+        if( ReferenceEquals(person1, person2)) return 0;    
+        if (person1 is null) return -1;
+        if (person2 is null) return 1;  
 
-        var max = Math.Max(x.GivenNames.Count, y.GivenNames.Count);
-        for (int i = 0; i < max; i++)
-        { 
-            var gx = i < x.GivenNames.Count ? x.GivenNames[i] : string.Empty;
-            var gy = i < y.GivenNames.Count ? y.GivenNames[i] : string.Empty;   
+        var result = _comp.Compare(person1.LastName, person2.LastName);
 
-            c = _comp.Compare(gx, gy);
-            if (c != 0) return c;
+        if(result != 0) return result;
+
+        var maxCount = Math.Max(person1.GivenNames.Count, person2.GivenNames.Count);
+
+        for (int i = 0; i < maxCount; i++)
+        {
+            var givenName1 = i < person1.GivenNames.Count ? person1.GivenNames[i] : string.Empty;
+            var givenName2 = i < person2.GivenNames.Count ? person2.GivenNames[i] : string.Empty;
+            result = _comp.Compare(givenName1, givenName2);
+            if (result != 0) return result;
         }
+
         return 0;
     }
 }
